29666225a031955ea45232681dcd8f75fcfdeb87
Patrick La Viola <patrickjlaviola@gmail.com>
2024-01-21 14:10:12 -0800

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Two Australian Shepards in a forest"
+        src='../images/aussies.png'
+      />
     </Layout>
   )
 }
